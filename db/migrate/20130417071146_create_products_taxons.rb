bc1faf2f01bc188a8e41436286c5dc5502bfb4d3
Yaron <justame@gmail.com>
2013-12-29 18:53:09 +0200

    updated for spree-2.1

--- db/migrate/20130417071146_create_products_taxons.rb
+++ db/migrate/20130417071146_create_products_taxons.rb
@@ -1,6 +1,5 @@
 class CreateProductsTaxons < ActiveRecord::Migration
   def change
-    add_column :spree_products_taxons, :id, :primary_key
     add_column :spree_products_taxons, :position, :integer, :default => 0
   end
 end
